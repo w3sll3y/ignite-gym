b4e932f50dbeaa0649f63805112443f2b885d735
Wesley Fernandes <wesley.fernandes@corebiz.ag>
2023-06-13 00:08:23 -0300

    first commit

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,70 @@
+import { useNavigation } from '@react-navigation/native';
+import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+
+import { AuthNavigationRouteProps } from '@routes/auth.routes';
+import LogoSvg from '@assets/logo.svg';
+import BackgroundImg from '@assets/background.png';
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
+
+
+export function SignIn() {
+
+  const navigation = useNavigation<AuthNavigationRouteProps>();
+
+  function handleNewAccount() {
+    navigation.navigate('signUp');
+  }
+
+  return (
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
+      <VStack flex={1} px={10} pb={16}>
+        <Image
+          source={BackgroundImg}
+          defaultSource={BackgroundImg}
+          alt="Pessoas treinando"
+          resizeMode="contain"
+          position="absolute"
+        />
+
+        <Center my={24}>
+          <LogoSvg />
+          <Text color="gray.100" fontSize="sm">
+            Treine sua mente e o seu corpo
+          </Text>
+        </Center>
+
+        <Center>
+          <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+            Acesse sua conta
+          </Heading>
+
+          <Input
+            placeholder="E-mail"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+
+          <Input
+            placeholder="Senha"
+            secureTextEntry
+          />
+
+          <Button title="Acessar" />
+        </Center>
+
+        <Center mt={24}>
+          <Text color="gray.100" fontSize="sm" mb={3} fontFamily="body">
+            Ainda nao tem acesso?
+          </Text>
+
+          <Button
+            title="Criar conta"
+            variant="outline"
+            onPress={handleNewAccount}
+          />
+        </Center>
+      </VStack>
+    </ScrollView>
+  )
+}
